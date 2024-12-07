56ef9aec4cc04339531f6f149112c59d010082e1
kevin cuadros <kevindavidcuadros@gmail.com>
2024-12-07 11:24:01 -0500

    Initial Commit

---
+++ Project-9-SwiftUI-challenge-activity-tracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Project-9-SwiftUI-challenge-activity-tracker
+//
+//  Created by Kevin Cuadros on 7/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Project-9-SwiftUI-challenge-activity-tracker/Project_9_SwiftUI_challenge_activity_trackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project_9_SwiftUI_challenge_activity_trackerApp.swift
+//  Project-9-SwiftUI-challenge-activity-tracker
+//
+//  Created by Kevin Cuadros on 7/12/24.
+//
+
+import SwiftUI
+
+@main
+struct Project_9_SwiftUI_challenge_activity_trackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
